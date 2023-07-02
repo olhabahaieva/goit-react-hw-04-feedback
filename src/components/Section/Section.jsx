c55b0cae2c423553b92f73067d6e5ecf459d8cbe
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-02 21:39:09 +0300

    Section added propTypes

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import css from './Section.module.css';
+import PropTypes from 'prop-types';
 
 const Section = ({ title, children }) => {
   return (
@@ -9,5 +10,9 @@ const Section = ({ title, children }) => {
     </div>
   );
 };
+
+Section.propTypes = {
+  title: PropTypes.string
+}
 
 export default Section;
