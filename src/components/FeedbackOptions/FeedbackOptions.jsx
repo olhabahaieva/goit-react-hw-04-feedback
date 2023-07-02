3d43c5686846dd76b2696a122dcd248e0e2bf0e5
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-02 21:20:00 +0300

    FeedbackOptions component transformed into functional Reacr component - no states or React methods were used

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,43 +1,41 @@
-import { Component } from 'react';
+import React from 'react';
 import css from './FeedbackOptions.module.css';
 
-class FeedbackOptions extends Component {
-  handleClick = buttonName => {
-    this.props.onFeedbackClick(buttonName);
+const FeedbackOptions = ({ onFeedbackClick }) => {
+  const handleClick = buttonName => {
+    onFeedbackClick(buttonName);
   };
 
-  render() {
-    return (
-      <>
-        <div className={css.buttonContainer}>
-          <button
-            name="good"
-            type="submit"
-            onClick={() => this.handleClick('good')}
-            className={css.button}
-          >
-            Good
-          </button>
-          <button
-            name="neutral"
-            type="submit"
-            onClick={() => this.handleClick('neutral')}
-            className={css.button}
-          >
-            Neutral
-          </button>
-          <button
-            name="bad"
-            type="submit"
-            onClick={() => this.handleClick('bad')}
-            className={css.button}
-          >
-            Bad
-          </button>
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div className={css.buttonContainer}>
+        <button
+          name="good"
+          type="submit"
+          onClick={() => handleClick('good')}
+          className={css.button}
+        >
+          Good
+        </button>
+        <button
+          name="neutral"
+          type="submit"
+          onClick={() => handleClick('neutral')}
+          className={css.button}
+        >
+          Neutral
+        </button>
+        <button
+          name="bad"
+          type="submit"
+          onClick={() => handleClick('bad')}
+          className={css.button}
+        >
+          Bad
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default FeedbackOptions;
