cb1e827c9ce8bc26fc48eaca5a7a67fb86347b2f
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-02 21:36:29 +0300

    Feedback component added props

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Section from 'components/Section';
 import FeedbackOptions from 'components/FeedbackOptions';
 import css from './Feedback.module.css';
+import PropTypes from 'prop-types';
 
 const Feedback = ({onFeedbackClick})=>{
   const handleClick = (buttonName) => {
@@ -18,5 +19,8 @@ const Feedback = ({onFeedbackClick})=>{
   );
 }
 
+Feedback.propTypes = {
+  onFeedbackClick: PropTypes.func
+}
 
 export default Feedback;
